7beef1d2f9ac2c1bc56d1331872f9b064b8de7de
Toni <garcia.t20@gmail.com>
2022-01-31 21:57:35 +0100

    create (post) of new events with api and async js

--- src/resources/events-form.js
+++ src/resources/events-form.js
@@ -1,9 +1,23 @@
 /*Import of events object and the eventsGeneration() function from events.js*/
-import {events} from "../js/objects.js";
 import {eventsGeneration} from "../js/events.js";
 
+/*
+ * API and fetch to READ data!
+ * Generating variable "events" with the information of the information saved
+ * Used at function takeValuesForm()
+ */
+var eventsUrl = "http://localhost:3000/events";
+
+async function getData(eventsUrl) {
+    const response = await fetch(eventsUrl);
+    return await response.json();
+}
+
+const events = await getData(eventsUrl);
+console.log(events);
+
 try{
-    /*Display the form to add a new event*/
+    /* Display the form to add a new event */
     document.getElementById("button-add-event").addEventListener("click", function () {
         document.getElementById("events-form-container").style.display = "flex"
         /*Date picker for the date input with JQuery*/
@@ -23,7 +37,6 @@ try{
         let closeButton = document.getElementsByClassName("close");
         for (let i = 0; i < closeButton.length; i++) {
             closeButton[i].addEventListener("click", function () {
-                console.log("Hola")
                 document.getElementById("events-form-container").style.display = "none";
             });
         }
@@ -31,29 +44,40 @@ try{
 
     function takeValuesForm() {
 
-        /*Taking the value of the inputs*/
+        /* Taking the value of the inputs */
         let title = document.getElementById("title").value
         let place = document.getElementById("place").value
         let description = document.getElementById("description").value
         let price = document.getElementById("price").value
         let date = document.getElementById("date").value
         let id = Math.round(Math.random() * 1000000)
-        /*Pushing the fields into events array with object json format*/
-        events.push({
-            id: id, name: title, date: date, place: place,
-            price: price, description: description, img: "./images/cinema-festival-vertical-poster-3451302.jpg"
+
+        /* Fetch to POST the new event at the API */
+        fetch('http://localhost:3000/events', {
+            method: 'POST',
+            body: JSON.stringify({
+                id: id,
+                name: title,
+                date: date,
+                place: place,
+                price: price,
+                description: description,
+                img: "./images/cinema-festival-vertical-poster-3451302.jpg"
+            }),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
         })
-        /*Calling eventsGeneration() with the new ones*/
-        eventsGeneration(events.length)
+            .then(response => response.json())
+            .then(json => console.log(json))
 
         document.getElementById("events-form-container").style.display = "none";
 
         /*Showing the content of the confirmation*/
         setTimeout('document.getElementById("form-sended").style.display = \"flex\"')
-        setTimeout('document.getElementById("form-sended").style.display = \"none\"', 2500)
-        document.getElementById("events-form").reset()
-
-
+        setTimeout('document.getElementById("form-sended").style.display = \"none\"', 1200)
+        document.getElementById("events-form").reset();
+        setTimeout('location.reload()', 2000);
     }
 }catch (e){
     console.log("Failed to load")
