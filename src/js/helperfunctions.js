15170d8b2c5eac8e483b4b370710a3a753ca6c90
Toni <garcia.t20@gmail.com>
2022-02-02 08:30:16 +0100

    added cookie functions on new file

---
+++ src/js/helperfunctions.js
@@ -0,0 +1,23 @@
+//Helper function for set a cookie
+export function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+//Helper function for query a cookie
+export function getCookie(cname) {
+    let name = cname + "=";
+    let ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) === ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) === 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
